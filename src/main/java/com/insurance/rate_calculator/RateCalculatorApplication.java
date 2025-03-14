0d5e7f068749a602bf596c7f003c8320a5ef898b
Uday Raghavan <udayragh@gmail.com>
2025-03-14 00:38:17 -0400

    base springboot project added

---
+++ src/main/java/com/insurance/rate_calculator/RateCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.rate_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insurance/rate_calculator/RateCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.rate_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
